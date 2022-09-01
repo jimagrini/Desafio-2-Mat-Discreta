ec1586c5e7a5376fb1292b1b86f3e42c264519ce
jimagrini <juan.magrini@correo.ucu.edu.uy>
2022-09-01 12:16:49 -0300

    Subiendo el desafio

---
+++ src/Desafio2/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Library;
+Challenge Carlos = new Challenge();
+Carlos.Calculator(100);
---
+++ src/Library/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace Library;
+public class Challenge
+{
+    public void Calculator(int edad)
+    {
+        int Amount_Ahorro=0;
+        int Amount=0;
+        for(int Age=10; Age <=edad; Age++)
+        {
+            Amount+= Age+Amount_Ahorro;
+            Amount_Ahorro+=Age;
+
+        }
+        Console.WriteLine($"Carlos desde los 10 años hasta los {edad}, logro ahorrar ${Amount}");
+    }
+
+}
